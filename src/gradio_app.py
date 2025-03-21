92ccd6cdf4a53f4a9cb70faba375ebed2f332a78
shouhei1310 <kdru1310@gmail.com>
2025-03-21 14:40:13 +0900

    openai連携完了

--- src/gradio_app.py
+++ src/gradio_app.py
@@ -1,7 +1,22 @@
 import gradio as gr
+from dotenv import load_dotenv
+from chatbot_engine import chat
 
-def greet(name):
-    return "Hello " + name + "!"
+def respond(message, chat_history):
+    bot_message = chat(message)
+    chat_history.append({"role": "user", "content": message})
+    chat_history.append({"role": "assistant", "content": bot_message})
+    return "", chat_history
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-demo.launch()   
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot(type="messages")
+    msg = gr.Textbox()
+    clear = gr.ClearButton([msg, chatbot])
+
+
+
+    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+
+if __name__ == "__main__":
+    load_dotenv()
+    demo.launch()
